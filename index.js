2670a961de6c667f0c7859d642a2c72954b4bb2e
b0gavmar <b0gavmar@vasvari.org>
2024-12-05 12:10:14 +0100

    error handlig + startServer

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+const startServer = async () => {
+    //await initializeDB();
+    app.listen(3000, () => console.log("Server is running on port 3000"));
+};
+
+startServer();
