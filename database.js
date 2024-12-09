2ca4694aaa9b5abf8833ca274db4ca5f87b70426
b0gavmar <b0gavmar@vasvari.org>
2024-12-09 17:57:08 +0100

    adatbazis mukodik

--- database.js
+++ database.js
@@ -3,17 +3,19 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("./database.sqlite");
 
 const initDB = async () => {
-    await dbRun("DROP TABLE users");
-    await dbRun("CREATE TABLE IF NOT EXISTS users (id INTEGER AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT PRIMARY KEY, class TEXT)");
+    await dbRun("DROP TABLE IF EXISTS users");
+    await dbRun("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT, lastName TEXT, email TEXT, class TEXT)");
 
 
     const users = [
         { firstName: "John", lastName: "Doe", email: "john.doe@example.com" , class: "13b"},
+        { firstName: "Jane",lastName:"Smith", email: "jane.smith@example.com", class: "12a"},
+        { firstName: "Sam",lastName:"Johnson", email: "sam.johnson@example.com" , class: "11c"},
     ];
 
-     for (const user of users) {
-        await dbRun("INSERT INTO users (firstName, lastName, email, class) VALUES (?, ?, ?, ?)", [user.firstName, user.lastName, user.email, user.class]);
-     }
+    for (const user of users) {
+       await dbRun("INSERT INTO users (firstName, lastName, email, class) VALUES (?, ?, ?, ?)", [user.firstName, user.lastName, user.email, user.class]);
+    }
 };
 
 function dbQuery(sql, params = []) {
@@ -34,4 +36,4 @@ function dbRun(sql, params = []) {
     });
 }
 
-export { db, dbQuery, dbRun, initializeDB };
+export { db, dbQuery, dbRun, initDB };
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { initDB } from "./database.js";
+import usersRouter from "./routes/users.js";
+import { readFile } from "fs/promises";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", usersRouter);
 
-//error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: err.message });
 });
 
 const startServer = async () => {
-    //await initializeDB();
+    await initDB();
     app.listen(3000, () => console.log("Server is running on port 3000"));
 };
 
